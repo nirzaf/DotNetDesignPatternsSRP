c69402999b51c60b14a1f4eebd289b88df7809b7
M F M Fazrin <mfmfazrin1986@gmail.com>
2022-01-10 15:02:47 +0530

    Liskov Substitution Principal : Derived Class should not affect the properties or  behaviours or Base Class

--- LiskovSubstitutionPrincipal/Program.cs
+++ LiskovSubstitutionPrincipal/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+namespace LiskovSubstitutionPrincipal;
+
+public class Program
+{
+    private static int Area(Rectangle r)
+    {
+        return r.Width * r.Height;
+    }
+
+    private static void Main(string[] args)
+    {
+        var rc = new Rectangle(2, 3);
+        WriteLine($"{rc} has area {Area(rc)}");
+
+        // should be able to substitute a base type for a subtype
+        /*Square*/
+        var sq = new Square
+        {
+            Width = 0,
+            Height = 0
+        };
+        sq.Width = 4;
+        WriteLine($"{sq} has area {Area(sq)}");
+        ReadLine();
+    }
+}
