2c789fd52340c46adf032b75363c65ce9128fd42
M F M Fazrin <mfmfazrin1986@gmail.com>
2022-01-10 17:08:00 +0530

    //High Level components should not depend on low level components

---
+++ DependencyInversion/Person.cs
@@ -0,0 +1,52 @@
+﻿public class Person
+{
+    public string Name { get; set; }
+
+    public interface IRelationshipBrowser
+    {
+        IEnumerable<Person> FindAllChildrenOf(string name);
+    }
+
+    public class Relationships : IRelationshipBrowser // low-level
+    {
+        private List<(Person, Relationship, Person)> relations = new();
+
+        public List<(Person, Relationship, Person)> Relations => relations;
+
+        public IEnumerable<Person> FindAllChildrenOf(string name)
+        {
+            return relations
+                .Where(x => x.Item1.Name == name
+                            && x.Item2 == Relationship.Parent).Select(r => r.Item3);
+        }
+
+        public void AddParentAndChild(Person parent, Person child)
+        {
+            relations.Add((parent, Relationship.Parent, child));
+            relations.Add((child, Relationship.Child, parent));
+        }
+    }
+
+    public class Research
+    {
+        public Research(Relationships relationships)
+        {
+            // high-level: find all of john's children
+            //var relations = relationships.Relations;
+            //foreach (var r in relations
+            //  .Where(x => x.Item1.Name == "John"
+            //              && x.Item2 == Relationship.Parent))
+            //{
+            //  WriteLine($"John has a child called {r.Item3.Name}");
+            //}
+        }
+
+        public Research(IRelationshipBrowser browser)
+        {
+            foreach (var p in browser.FindAllChildrenOf("John"))
+            {
+                Console.WriteLine($"John has a child called {p.Name}");
+            }
+        }
+    }
+}
--- DependencyInversion/Program.cs
+++ DependencyInversion/Program.cs
@@ -1,4 +1,13 @@
 ﻿//High Level components should not depend on low level components 
 
-
 Console.WriteLine("Hello, World!");
+var parent = new Person { Name = "John" };
+var child1 = new Person { Name = "Chris" };
+var child2 = new Person { Name = "Matt" };
+
+// low-level module
+var relationships = new Person.Relationships();
+relationships.AddParentAndChild(parent, child1);
+relationships.AddParentAndChild(parent, child2);
+
+var research = new Person.Research(relationships);
