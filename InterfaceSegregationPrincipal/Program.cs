57d8179d185fd3efc74f98c024b7436f91816806
M F M Fazrin <mfmfazrin1986@gmail.com>
2022-01-10 16:10:05 +0530

    //Interface should not force the implementation when it is not required to implement

--- InterfaceSegregationPrincipal/Program.cs
+++ InterfaceSegregationPrincipal/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//Interface should not force the implementation when it is not required to implement 
 
 Console.WriteLine("Hello, World!");
 
