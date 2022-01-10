5ec300280ef424d074d4b89b305a6bcb4569b285
M F M Fazrin <mfmfazrin1986@gmail.com>
2022-01-10 14:39:21 +0530

    Single Resposibility Pattern

---
+++ DotNetDesignPatternsSRP/Journal.cs
@@ -0,0 +1,20 @@
+﻿public class Journal
+{
+    private static int _count = 0;
+    private readonly List<string> _entries = new();
+
+    public void AddEntry(string text)
+    {
+        _entries.Add($"{++_count}: {text}");
+    }
+
+    public void RemoveEntry(int index)
+    {
+        _entries.RemoveAt(index);
+    }
+
+    public override string ToString()
+    {
+        return string.Join(Environment.NewLine, _entries);
+    }
+}
---
+++ DotNetDesignPatternsSRP/Persistence.cs
@@ -0,0 +1,15 @@
+﻿public class Persistence
+{
+    public void SaveToFile(Journal journal, string filename, bool overwrite = false)
+    {
+        try
+        {
+            if (overwrite || !File.Exists(filename))
+                File.WriteAllText(filename, journal.ToString());
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+}
